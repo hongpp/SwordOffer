d269cf1eeccc53e945f5ac65bf4faa0012a67b94
yeake <zuqiuyu@foxmail.com>
2019-01-07 14:22:45 +0800

    初始版本

--- src/Important1/P62_ConstructBinaryTree.java
+++ src/Important1/P62_ConstructBinaryTree.java
@@ -52,17 +52,14 @@ public class P62_ConstructBinaryTree {
 
 
     public static TreeNode reConstructBinaryTree(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn) {
-        if (pre == null || in == null) {
-            return null;
-        }
         if (startPre > endPre || startIn > endIn) {
             return null;
         }
         TreeNode root = new TreeNode(pre[startPre]);
         for (int i = 0; i < in.length; i++) {
-            if ((int) root.val == in[i]) {
-                root.left = reConstructBinaryTree(pre, startPre + 1, (i - startIn) + startPre, in, startIn, i - 1);
-                root.right = reConstructBinaryTree(pre, (i - startIn) + startPre + 1, endPre, in, i + 1, endIn);
+            if (in[i] == (int) root.val) {
+                root.left = reConstructBinaryTree(pre, startPre + 1, startPre + (i - startIn), in, startIn, i - 1);
+                root.right = reConstructBinaryTree(pre, startPre + (i - startIn) + 1, endPre, in, i + 1, endIn);
             }
         }
         return root;
