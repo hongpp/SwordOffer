085b7719d49463f75bf870fc540aa9c30636e6ab
yeake <zuqiuyu@foxmail.com>
2019-01-08 13:31:31 +0800

    初始版本

--- src/Important1/P271_TreeDepth.java
+++ src/Important1/P271_TreeDepth.java
@@ -6,6 +6,21 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class P271_TreeDepth {
+    private boolean isBalanced = true;
+
+    /**
+     * 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+     * 已过牛客
+     * <p>
+     * https://www.nowcoder.com/practice/8b3b95850edb4115918ecebdf1b4d222?tpId=13&tqId=11192&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+     *
+     * @param root
+     * @return
+     */
+    public boolean IsBalanced_Solution(TreeNode root) {
+        TreeDepth(root);
+        return isBalanced;
+    }
     /**
      * 已过牛客
      * <p>
@@ -14,26 +29,16 @@ public class P271_TreeDepth {
      * @param root
      * @return
      */
-    public static int TreeDepth(TreeNode root) {
+    public int TreeDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
         int left = TreeDepth(root.left);
         int right = TreeDepth(root.right);
+        if (Math.abs(left - right) > 1) {
+            isBalanced = false;
+        }
         return Math.max(left, right) + 1;
-    }
-
-    public static void main(String[] args) {
-        TreeNode<Integer> root = new TreeNode<>(1);
-        root.left = new TreeNode<>(2);
-        root.left.left = new TreeNode<>(4);
-        //root.left.left.left = new TreeNode<>(8);
-        root.left.right = new TreeNode<>(5);
-        root.left.right.left = new TreeNode<>(7);
-        root.right = new TreeNode<>(3);
-        root.right.right = new TreeNode<>(6);
-        System.out.println(TreeDepth(root));
-
     }
 
 }
